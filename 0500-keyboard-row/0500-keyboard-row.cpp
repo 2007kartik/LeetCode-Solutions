7ab80fcac4dd30164b708d6fb60062de156ac7d1
Kartik A. Mahanor <mahanorkartik372@gmail.com>
2026-07-01 09:47:52 +0530

    Time: 0 ms (100%), Space: 9.1 MB (49.53%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -1,52 +1,26 @@
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
+        unordered_map<char,int> m;
+        string one = "qwertyuiopQWERTYUIOP";
+        string two = "asdfghjklASDFGHJKL";
+        string three = "zxcvbnmZXCVBNM";
+        for(char& ch :one) m[ch] =1;
+        for(char& ch :two) m[ch] =2;
+        for(char& ch :three) m[ch] =3;
 
-        unordered_set<char> row1 = {'q','w','e','r','t','y','u','i','o','p'};
-        unordered_set<char> row2 = {'a','s','d','f','g','h','j','k','l'};
-        unordered_set<char> row3 = {'z','x','c','v','b','n','m'};
-
-        vector<string> ans;
-
-        for (string word : words) {
-
-            int row = 0;
-
-            char first = tolower(word[0]);
-
-            if (row1.count(first))
-                row = 1;
-            else if (row2.count(first))
-                row = 2;
-            else
-                row = 3;
-
-            bool ok = true;
-
-            for (char ch : word) {
-
-                ch = tolower(ch);
-
-                if (row == 1 && !row1.count(ch)) {
-                    ok = false;
-                    break;
-                }
-
-                if (row == 2 && !row2.count(ch)) {
-                    ok = false;
-                    break;
-                }
-
-                if (row == 3 && !row3.count(ch)) {
-                    ok = false;
+        vector<string>v;
+        for(string &str:words){
+            int ind = m[str[0]];
+            bool flag = true;
+            for(char &ch: str){
+                if(m[ch] != ind){
+                    flag = false;
                     break;
                 }
             }
-
-            if (ok)
-                ans.push_back(word);
+            if(flag) v.push_back(str);
         }
-
-        return ans;
+        return v;
     }
 };

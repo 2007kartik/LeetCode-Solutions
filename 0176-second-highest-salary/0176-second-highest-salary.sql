22cdc36454a8393aea6c77a79cdb51222e84e0a0
Kartik A. Mahanor <mahanorkartik372@gmail.com>
2026-08-30 16:11:12 +0530

    Time: 252 ms (97.86%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select max(salary) as SecondHighestSalary 
+from (
+    select salary, dense_rank() over(
+        order by salary desc
+    ) as rnk
+    from Employee
+) t
+where rnk =2;

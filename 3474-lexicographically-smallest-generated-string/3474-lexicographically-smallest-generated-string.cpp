fd1e28d196614ddf97465a7e7fbb8cdcb406448f
Kartik A. Mahanor <mahanorkartik372@gmail.com>
2026-08-31 09:00:56 +0530

    Time: 11 ms (90.21%), Space: 11.1 MB (81.36%) - LeetHub

--- 3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.cpp
+++ 3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.cpp
@@ -1,53 +1,128 @@
 class Solution {
 public:
+
+    // Checks whether:
+    // word[i ... i + m - 1] == str2
+    bool isSame(string &word, string &str2, int i, int m) {
+
+        for (int j = 0; j < m; j++) {
+
+            if (word[i + j] != str2[j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
     string generateString(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        int len = n + m - 1;
-        string ans(len, 'a');
-        vector<bool> fixed(len, false);
-        // Step 1: Handle all 'T'
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int N = n + m - 1;
+
+        // Initially all positions are '$'
+        string word(N, '$');
+
+        // true means this position can still be changed
+        vector<bool> canChange(N, false);
+
+
+        // ------------------------------------------------
+        // STEP 1: Process all 'T'
+        // ------------------------------------------------
+
         for (int i = 0; i < n; i++) {
-            if (str1[i] != 'T')
-                continue;
-            for (int j = 0; j < m; j++) {
-                int pos = i + j;
-                // Conflict with an already fixed position
-                if (fixed[pos] && ans[pos] != str2[j])
-                    return "";
-                ans[pos] = str2[j];
-                fixed[pos] = true;
-            }
-        }
-        // Step 2: Handle all 'F'
-        for (int i = 0; i < n; i++) {
-            if (str1[i] != 'F')
-                continue;
-            // Check whether current substring equals str2
-            bool same = true;
-            for (int j = 0; j < m; j++) {
-                if (ans[i + j] != str2[j]) {
-                    same = false;
-                    break;
+
+            if (str1[i] == 'T') {
+
+                int i_ = i;
+
+                for (int j = 0; j < m; j++) {
+
+                    // If already fixed and different -> impossible
+                    if (word[i_] != '$' &&
+                        word[i_] != str2[j]) {
+
+                        return "";
+                    }
+
+                    word[i_] = str2[j];
+
+                    i_++;
                 }
             }
-            // Already different -> F condition satisfied
-            if (!same)
-                continue;
-            // Need to make it different.
-            // Change the rightmost unfixed position.
-            bool changed = false;
-            for (int j = i + m - 1; j >= i; j--) {
-                if (!fixed[j]) {
-                    ans[j] = 'b';
-                    changed = true;
-                    break;
+        }
+
+
+        // ------------------------------------------------
+        // STEP 2: Fill remaining positions with 'a'
+        // ------------------------------------------------
+
+        for (int i = 0; i < N; i++) {
+
+            if (word[i] == '$') {
+
+                word[i] = 'a';
+
+                // This position was not fixed by T,
+                // so we are allowed to change it later.
+                canChange[i] = true;
+            }
+        }
+
+
+        // ------------------------------------------------
+        // STEP 3: Process all 'F'
+        // ------------------------------------------------
+
+        for (int i = 0; i < n; i++) {
+
+            if (str1[i] == 'F') {
+
+                // If the current substring is already
+                // different from str2, F condition is satisfied.
+
+                if (isSame(word, str2, i, m)) {
+
+                    bool changed = false;
+
+
+                    // We need to make this substring different.
+                    //
+                    // Start from the RIGHTMOST position because
+                    // that gives the lexicographically smallest
+                    // possible answer.
+
+                    for (int k = i + m - 1; k >= i; k--) {
+
+                        if (canChange[k] == true) {
+
+                            word[k] = 'b';
+
+                            changed = true;
+
+                            break;
+                        }
+                    }
+
+
+                    // Every character of this substring was
+                    // fixed by a T condition.
+                    //
+                    // Therefore we cannot make it different.
+
+                    if (changed == false) {
+
+                        return "";
+                    }
                 }
             }
-            // Every position was fixed by T
-            if (!changed)
-                return "";
         }
-        return ans;
+
+
+        return word;
     }
 };

64243cbf7f4a68fa36c7acf67dfe42f89ec5f4ae
Kartik A. Mahanor <mahanorkartik372@gmail.com>
2026-09-07 19:11:09 +0530

    Time: 23 ms (22.22%), Space: 20.3 MB (19.41%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,40 +2,63 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
 
-        vector<vector<int>> dist(n , vector<int>(n , INT_MAX));
-        for(auto it : edges){
-            dist[it[0]][it[1]] = it[2];
-            dist[it[1]][it[0]] = it[2];
+        //create a adjlist
+        vector<vector<pair<int,int>>> adjlist(n);
+
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adjlist[u].push_back({v,wt});
+            adjlist[v].push_back({u,wt});
+
         }
-        //for [i][i] = 0
-        for(int i  = 0;i<n;i++) dist[i][i] = 0;
 
-        //Apply floyd-warshall algo
-        for(int k = 0;k<n;k++){
-            for(int i =0;i<n;i++){
-                for(int j = 0;j<n;j++){
-                    if(dist[i][k]==INT_MAX||dist[k][j]==INT_MAX) continue;
+        int mincount = INT_MAX;
+        int ans = -1;
 
-                    dist[i][j] = min(dist[i][j]  , dist[i][k]+dist[k][j]);
+        for(int scr =0;scr<n;scr++){
+
+            //dijstra algo needs , distance array and min heap
+            vector<int> dist(n , INT_MAX);
+
+            priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int,int>>> pq;
+            pq.push({0 , scr});
+            dist[scr] =0;
+            while(!pq.empty()){
+                int d = pq.top().first;
+                int node = pq.top().second;
+                pq.pop();
+                //ignore the outed entry in the pq
+                if(d>dist[node]) continue;
+                if(d>distanceThreshold) continue;
+
+                for(auto it : adjlist[node]){
+
+                    int next = it.first;
+                    int wt = it.second;
+
+                    if(d + wt < dist[next]){
+                        dist[next] = wt+d;
+                        pq.push({dist[next] , next});
+                    }
                 }
             }
-        }
 
-        int cntCity = n;
-        int cityno = -1;
-        for(int city = 0;city<n;city++){
             int cnt = 0;
-            for(int adjcity = 0;adjcity<n;adjcity++){
-                if(dist[city][adjcity]<=distanceThreshold){
+            for(int i  = 0;i<n;i++){
+                if(i!=scr && dist[i]<=distanceThreshold){
                     cnt++;
                 }
             }
-            if(cnt<=cntCity){
-                cntCity = cnt;
-                cityno =city;
+            if(cnt<=mincount){
+                mincount = cnt;
+                ans = scr;
             }
+
         }
+        return ans;
         
-        return cityno;
     }
 };
